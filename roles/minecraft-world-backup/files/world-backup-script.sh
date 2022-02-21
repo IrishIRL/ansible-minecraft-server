e3de69eed8382020befc9f1f5c2d659dd9620853
Mark Shafran <medaluckf@gmail.com>
2022-02-21 11:44:12 -0800

    Minor fix

--- roles/minecraft-world-backup/files/world-backup-script.sh
+++ roles/minecraft-world-backup/files/world-backup-script.sh
@@ -7,5 +7,5 @@ tar -zcvpf $BACKUP_DIR/world-$DATE.tar.gz /minecraft-data/world
 tar -zcvpf $BACKUP_DIR/world_nether-$DATE.tar.gz /minecraft-data/world_nether
 tar -zcvpf $BACKUP_DIR/world_the_end-$DATE.tar.gz /minecraft-data/world_the_end
 
-# Delete files older than 10 days #
+# Delete files older than 3 days #
 find $BACKUP_DIR/* -mtime +3 -exec rm {} \;
